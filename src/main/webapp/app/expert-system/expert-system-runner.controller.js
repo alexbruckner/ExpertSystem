7174f260802f9d50dc34f1f95f4d471e754331a3
Bruckner, Alexander <Alexander.Bruckner@zuehlke.com>
2017-01-16 16:47:19 +0100

    expert system runner: css, back button

--- src/main/webapp/app/expert-system/expert-system-runner.controller.js
+++ src/main/webapp/app/expert-system/expert-system-runner.controller.js
@@ -10,9 +10,13 @@
     function ExpertSystemRunnerController (ExpertSystem, $stateParams, Answer) {
         var vm = this;
         vm.selectedAnswer = selectedAnswer;
+        vm.concluded = false;
         vm.restart = restart;
+        vm.back = back;
 
         ExpertSystem.get({id : $stateParams.id}, onSuccess, onError);
+
+        vm.questions = [];
 
         function onSuccess(data) {
             vm.expertSystem = data;
@@ -23,21 +27,31 @@
             newQuestion(vm.expertSystem.question);
         }
 
+        function back() {
+            if (vm.questions.length > 0) {
+                newQuestion(vm.questions.pop());
+            }
+        }
+
         function onError(error) {
             alert(error.data.message);
         }
 
         function selectedAnswer(answer) {
+            vm.questions.push(answer.question);
             if (answer.conclusion.question) {
                 newQuestion(answer.conclusion.question);
             } else {
                 vm.item = answer.conclusion;
                 vm.answers = null;
+                vm.concluded = true;
             }
         }
 
         function newQuestion(question){
+            vm.concluded = false;
             vm.item = question;
+
             Answer.forQuestion({id : vm.item.id}, onAnswerSuccess, onError);
 
             function onAnswerSuccess(data) {
